1333aa8c73bdb3f25325b0fcf397033909ede028
Nekromateion <nekromateion@web.de>
2020-09-22 12:50:41 +0200

    idk

---
+++ ArtikelverwaltungListStruct/Artikel.cs
@@ -0,0 +1,10 @@
+namespace ArtikelverwaltungListStruct
+{
+    public struct Artikel
+    {
+         public int nummer { get; set; }
+         public string name { get; set; }
+         public double preis { get; set; }
+         public int bestand { get; set; }
+    }
+}
--- ArtikelverwaltungListStruct/Program.cs
+++ ArtikelverwaltungListStruct/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace ArtikelverwaltungListStruct
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArtikelverwaltungListStruct
 {
     internal class Program
     {
+        private static string waerung = String.Empty;
+        private static List<Artikel> _artikel = new List<Artikel>();
+        
         public static void Main(string[] args)
         {
+            Console.WriteLine("Bitte ");
         }
     }
 }
