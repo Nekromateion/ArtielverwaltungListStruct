e06f7b712c7c4a8a8d4dbaa80c1b1b7e026c69e6
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-02 18:30:16 +0200

    Update Program.cs

--- ArtikelverwaltungListStructClientConsole/Program.cs
+++ ArtikelverwaltungListStructClientConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ArtikelverwaltungListStructClientConsole
 {
@@ -6,10 +7,49 @@ namespace ArtikelverwaltungListStructClientConsole
     {
         public static void Main(string[] args)
         {
-            Console.Write("Please input the Servers IP: ");
-            string serverip = Console.ReadLine();
-            Console.Write("Please input the Servers Port: ");
-            string serverport = Console.ReadLine();
+            bool serverAvailable = false;
+            while (!serverAvailable)
+            {
+                Console.Write("Please input the Servers IP: ");
+                string serverip = Console.ReadLine();
+                Console.Write("Please input the Servers Port: ");
+                string serverport = Console.ReadLine();
+                Console.Clear();
+                Thread writeThread = new Thread(() =>
+                {
+                    int delay = 50;
+                    while (true)
+                    {
+                        Console.WriteLine("Checking if server is reachable -");
+                        Thread.Sleep(delay);
+                        Console.Clear();
+                        Console.WriteLine("Checking if server is reachable \\");
+                        Thread.Sleep(delay);
+                        Console.Clear();
+                        Console.WriteLine("Checking if server is reachable |");
+                        Thread.Sleep(delay);
+                        Console.Clear();
+                        Console.WriteLine("Checking if server is reachable /");
+                        Thread.Sleep(delay);
+                        Console.Clear();
+                        Console.WriteLine("Checking if server is reachable -");
+                        Thread.Sleep(delay);
+                        Console.Clear();
+                        Console.WriteLine("Checking if server is reachable \\");
+                        Thread.Sleep(delay);
+                        Console.Clear();
+                        Console.WriteLine("Checking if server is reachable |");
+                        Thread.Sleep(delay);
+                        Console.Clear();
+                        Console.WriteLine("Checking if server is reachable /");
+                        Thread.Sleep(delay);
+                        Console.Clear();
+                    }
+                });
+                writeThread.Start();
+            }
+            Console.WriteLine("Please select one of the following options");
+            Console.WriteLine("1: Read Current list");
         }
     }
 }
