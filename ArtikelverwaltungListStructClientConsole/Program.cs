fe4881c7d64d28a8e8367dd8fab0284025790008
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-02 19:27:29 +0200

    Update Program.cs

--- ArtikelverwaltungListStructClientConsole/Program.cs
+++ ArtikelverwaltungListStructClientConsole/Program.cs
@@ -14,97 +14,102 @@ namespace ArtikelverwaltungListStructClientConsole
         private static bool doRun = true;
         public static void Main(string[] args)
         {
-            while (!serverAvailable)
-            {
-                Console.Write("Please input the Servers IP: ");
-                string serverip = Console.ReadLine();
-                Console.Write("Please input the Servers Port: ");
-                string serverport = Console.ReadLine();
-                Console.Clear();
-                Thread writeThread = new Thread(() =>
-                {
-                    int delay = 50;
-                    while (true)
-                    {
-                        Console.WriteLine("Checking if server is reachable -");
-                        Thread.Sleep(delay);
-                        Console.Clear();
-                        Console.WriteLine("Checking if server is reachable \\");
-                        Thread.Sleep(delay);
-                        Console.Clear();
-                        Console.WriteLine("Checking if server is reachable |");
-                        Thread.Sleep(delay);
-                        Console.Clear();
-                        Console.WriteLine("Checking if server is reachable /");
-                        Thread.Sleep(delay);
-                        Console.Clear();
-                        Console.WriteLine("Checking if server is reachable -");
-                        Thread.Sleep(delay);
-                        Console.Clear();
-                        Console.WriteLine("Checking if server is reachable \\");
-                        Thread.Sleep(delay);
-                        Console.Clear();
-                        Console.WriteLine("Checking if server is reachable |");
-                        Thread.Sleep(delay);
-                        Console.Clear();
-                        Console.WriteLine("Checking if server is reachable /");
-                        Thread.Sleep(delay);
-                        Console.Clear();
-                    }
-                });
-                writeThread.Start();
-                try
-                {
-                    string response = new WebClient().DownloadString($"http://{serverip}:{serverport}/status");
-                    writeThread.Abort();
-                    if (response != "0")
-                    {
-                        Console.Clear();
-                        Console.WriteLine("The given server was reachable but is not a valid server type in a valid ip and port...");
-                        Thread.Sleep(2500);
-                        Console.Clear();
-                    }
-                    else
-                    {
-                        Console.Clear();
-                        serverAvailable = true;
-                        _serverIp = serverip;
-                        _serverPort = serverport;
-                    }
-                }
-                catch (Exception)
-                {
-                    writeThread.Abort();
-                    Console.Clear();
-                    Console.WriteLine("The given server is unreachable please type in a valid ip and port...");
-                    Thread.Sleep(2500);
-                    Console.Clear();
-                }
-            }
-            _currency = new WebClient().DownloadString($"http://{_serverIp}:{_serverPort}/curr");
-
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine("Please select one of the following options");
-                Console.WriteLine("e: Exit the application");
-                Console.WriteLine("c: Close the server");
-                Console.WriteLine("1: Read Current list");
-                Console.WriteLine("2: Add a new article");
-                Console.WriteLine("3: Delete a article");
-                Console.WriteLine("");
-                Console.Write("Your input: ");
-                string input = Console.ReadLine();
-            
-                if(input == "1" || input == "read" || input == "list") ReadList();
-                else if (input == "2" || input == "add" || input == "put") AddArticle();
-                else if (input == "3" || input == "remove" || input == "delete" || input == "del") DelArticle();
-                else if (input == "e" || input == "exit") Environment.Exit(0xDEAD);
-                else if (input == "c" || input == "close" || input == "abort") CloseServer();
-                else
+                doRun = true;
+                while (!serverAvailable)
                 {
-                    Console.WriteLine("That is not a valid input");
-                    Thread.Sleep(2500);
+                    Console.Write("Please input the Servers IP: ");
+                    string serverip = Console.ReadLine();
+                    Console.Write("Please input the Servers Port: ");
+                    string serverport = Console.ReadLine();
+                    Console.Clear();
+                    Thread writeThread = new Thread(() =>
+                    {
+                        int delay = 50;
+                        while (true)
+                        {
+                            Console.WriteLine("Checking if server is reachable -");
+                            Thread.Sleep(delay);
+                            Console.Clear();
+                            Console.WriteLine("Checking if server is reachable \\");
+                            Thread.Sleep(delay);
+                            Console.Clear();
+                            Console.WriteLine("Checking if server is reachable |");
+                            Thread.Sleep(delay);
+                            Console.Clear();
+                            Console.WriteLine("Checking if server is reachable /");
+                            Thread.Sleep(delay);
+                            Console.Clear();
+                            Console.WriteLine("Checking if server is reachable -");
+                            Thread.Sleep(delay);
+                            Console.Clear();
+                            Console.WriteLine("Checking if server is reachable \\");
+                            Thread.Sleep(delay);
+                            Console.Clear();
+                            Console.WriteLine("Checking if server is reachable |");
+                            Thread.Sleep(delay);
+                            Console.Clear();
+                            Console.WriteLine("Checking if server is reachable /");
+                            Thread.Sleep(delay);
+                            Console.Clear();
+                        }
+                    });
+                    writeThread.Start();
+                    try
+                    {
+                        string response = new WebClient().DownloadString($"http://{serverip}:{serverport}/status");
+                        writeThread.Abort();
+                        if (response != "0")
+                        {
+                            Console.Clear();
+                            Console.WriteLine("The given server was reachable but is not a valid server type in a valid ip and port...");
+                            Thread.Sleep(2500);
+                            Console.Clear();
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            serverAvailable = true;
+                            _serverIp = serverip;
+                            _serverPort = serverport;
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        writeThread.Abort();
+                        Console.Clear();
+                        Console.WriteLine("The given server is unreachable please type in a valid ip and port...");
+                        Thread.Sleep(2500);
+                        Console.Clear();
+                    }
+                }
+                _currency = new WebClient().DownloadString($"http://{_serverIp}:{_serverPort}/curr");
+
+                while (doRun)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please select one of the following options");
+                    Console.WriteLine("e: Exit the application");
+                    Console.WriteLine("c: Close the server");
+                    Console.WriteLine("1: Read Current list");
+                    Console.WriteLine("2: Add a new article");
+                    Console.WriteLine("3: Delete a article");
+                    Console.WriteLine("");
+                    Console.Write("Your input: ");
+                    string input = Console.ReadLine();
+            
+                    if(input == "1" || input == "read" || input == "list") ReadList();
+                    else if (input == "2" || input == "add" || input == "put") AddArticle();
+                    else if (input == "3" || input == "remove" || input == "delete" || input == "del") DelArticle();
+                    else if (input == "e" || input == "exit") Environment.Exit(0xDEAD);
+                    else if (input == "c" || input == "close" || input == "abort") CloseServer();
+                    else
+                    {
+                        Console.WriteLine("That is not a valid input");
+                        Thread.Sleep(2500);
+                    }
                 }
             }
         }
@@ -160,6 +165,8 @@ namespace ArtikelverwaltungListStructClientConsole
             {
                 Console.WriteLine("Server is closing...");
                 Thread.Sleep(2500);
+                doRun = false;
+                serverAvailable = false;
             }
         }
     }
