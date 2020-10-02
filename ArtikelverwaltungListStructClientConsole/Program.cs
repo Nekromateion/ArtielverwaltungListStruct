3f7c5ddd05649e839abb16a476d0cbad14b3da54
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-02 18:22:10 +0200

    Update Program.cs

--- ArtikelverwaltungListStructClientConsole/Program.cs
+++ ArtikelverwaltungListStructClientConsole/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace ArtikelverwaltungListStructClientConsole
+﻿using System;
+
+namespace ArtikelverwaltungListStructClientConsole
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.Write("Please input the Servers IP: ");
+            string serverip = Console.ReadLine();
+            Console.Write("Please input the Servers Port: ");
+            string serverport = Console.ReadLine();
         }
     }
 }
