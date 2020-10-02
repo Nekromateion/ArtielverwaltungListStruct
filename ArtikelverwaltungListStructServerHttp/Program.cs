cf6edacdecdbc756c4429f41bb89bff2af74d209
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-02 20:47:36 +0200

    Update Program.cs

--- ArtikelverwaltungListStructServerHttp/Program.cs
+++ ArtikelverwaltungListStructServerHttp/Program.cs
@@ -7,15 +7,20 @@ namespace ArtikelverwaltungListStruct
     internal class Program
     {
         public static string currency = String.Empty;
+        public static string Ip = String.Empty;
         
         public static void Main(string[] args)
         {
             Console.Title = "Artikelverwaltungs Server";
+            Console.Write("Please enter the IP you want the server to use:");
+            Ip = Console.ReadLine();
+            Console.Clear();
             Console.Write("Please input the server close key: ");
             string key = Console.ReadLine();
             Console.Clear();
             Console.Write("Please input the currency you want the server to use: ");
             currency = Console.ReadLine();
+            Console.Clear();
             Server server = new Server(8080, key);
         }
     }
