113aac54eb983dec0378711f7aa6024702aa9488
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-02 19:05:48 +0200

    Update Program.cs

--- ArtikelverwaltungListStructServerHttp/Program.cs
+++ ArtikelverwaltungListStructServerHttp/Program.cs
@@ -6,12 +6,16 @@ namespace ArtikelverwaltungListStruct
 {
     internal class Program
     {
+        public static string currency = String.Empty;
+        
         public static void Main(string[] args)
         {
             Console.Title = "Artikelverwaltungs Server";
             Console.Write("Please input the server close key: ");
             string key = Console.ReadLine();
             Console.Clear();
+            Console.Write("Please input the currency you want the server to use: ");
+            currency = Console.ReadLine();
             Server server = new Server(8080, key);
         }
     }
