6c1a21e7794cbc2dd955155b590541051a6e9cf0
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-03 23:14:05 +0200

    Update Logger.cs

--- ArtikelverwaltungListStructClientConsoleHttp/Logger.cs
+++ ArtikelverwaltungListStructClientConsoleHttp/Logger.cs
@@ -1,8 +1,12 @@
-﻿namespace ArtikelverwaltungListStructClientConsole
+﻿using System;
+using System.IO;
+
+namespace ArtikelverwaltungListStructClientConsole
 {
     public class Logger
     {
         public static string LogName { get; set; }
-        
+
+        private static string LogFile = Path.Combine(Path.Combine(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "NW"), "Artikelverwaltung"), LogName);
     }
 }
