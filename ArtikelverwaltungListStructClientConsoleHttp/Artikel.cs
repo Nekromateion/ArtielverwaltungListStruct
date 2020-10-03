0168aece35c1c7dc56eca294d75d931126ac1731
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-03 22:55:22 +0200

    Create Artikel.cs

---
+++ ArtikelverwaltungListStructClientConsoleHttp/Artikel.cs
@@ -0,0 +1,10 @@
+﻿namespace ArtikelverwaltungListStruct
+{
+    public struct Artikel
+    {
+        public int nummer { get; set; }
+        public string name { get; set; }
+        public double preis { get; set; }
+        public int bestand { get; set; }
+    }
+}
