67dbbc1018e55d94382f27aad566bf85091a5280
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-05 14:16:27 +0200

    Update Program.cs

--- ArtikelverwaltungListStructClientConsoleHttp/Program.cs
+++ ArtikelverwaltungListStructClientConsoleHttp/Program.cs
@@ -193,6 +193,7 @@ namespace ArtikelverwaltungListStructClientConsoleHttp
                     else if (input == "e" || input == "exit") Environment.Exit(0xDEAD);
                     else if (input == "c" || input == "close" || input == "abort"){ if(_key != string.Empty) CloseServer(); }
                     else if (input == "s" || input == "save" || input == "speichern"){if(_key != string.Empty) Save(); }
+                    else if (input == "cl" || input == "save" || input == "speichern"){if(_key != string.Empty) Save(); }
                     else
                     {
                         Console.WriteLine("That is not a valid input");
