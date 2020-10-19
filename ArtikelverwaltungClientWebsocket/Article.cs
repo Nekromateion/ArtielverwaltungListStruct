3238c29c39ec79d7fb1b58a6d546a8f3f598aeab
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-20 00:28:07 +0200

    added struct

---
+++ ArtikelverwaltungClientWebsocket/Article.cs
@@ -0,0 +1,11 @@
+﻿namespace ArtikelverwaltungClientWebsocket
+{
+    public struct Artikel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public int count { get; set; }
+        public byte[] attachedFile { get; set; } // this is used to store a file on the server (maybe a .pdf with extra infos)
+    }
+}
