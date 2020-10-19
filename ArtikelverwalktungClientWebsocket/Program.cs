0130630cc94100e1885599ea38415447f0b085d3
Nekromateion <nekromateion@web.de>
2020-10-19 02:21:16 +0200

    oh boi this is gonna be great xD

---
+++ ArtikelverwalktungClientWebsocket/Program.cs
@@ -0,0 +1,19 @@
+﻿using WebSocketSharp;
+
+namespace ArtikelverwalktungClientWebsocket
+{
+    public class SocketManager
+    {
+        public static WebSocket Socket = new WebSocket("ws://localhost/artikelverwaltung");
+    }
+    
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        internal static byte[] assembly;
+    }
+}
