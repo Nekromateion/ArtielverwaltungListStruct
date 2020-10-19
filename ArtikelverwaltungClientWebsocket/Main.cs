028a5334cb3bd7bc3b0b000e1a4fece0ad38539b
Nekromateion <nekromateion@web.de>
2020-10-19 05:33:54 +0200

    Update Main.cs

--- ArtikelverwaltungClientWebsocket/Main.cs
+++ ArtikelverwaltungClientWebsocket/Main.cs
@@ -10,18 +10,43 @@ namespace ArtikelverwaltungClientWebsocket
     
     public class Main
     {
+        private static ArtikelverwaltungClientWebsocketLoader.Logger logger =
+            ArtikelverwaltungClientWebsocketLoader.LogHandler.logger;
         public static void OnApplicationStart()
         {
+            logger.AddLine("Called");
+            logger.AddLine("Getting webhook");
             Console.WriteLine("Getting webhook from loader");
             ConnectionManager.socket = ArtikelverwaltungClientWebsocketLoader.SocketManager.Socket;
-            Console.WriteLine("Got socket");
+            Console.WriteLine("Got webhook");
+            logger.AddLine("Got webhook");
+            logger.AddLine("setting up methods");
             Console.WriteLine("Setting up methods");
             ConnectionManager.socket.OnMessage += OnMessage;
+            logger.AddLine("method setup done");
         }
 
         private static void OnMessage(object sender, MessageEventArgs e)
         {
             Console.WriteLine("Received message from server");
+            logger.AddLine("received message from server");
+            if (e.IsText)
+            {
+                logger.AddLine("message was text");
+                logger.AddLine("received data: " + e.Data);
+            }
+            else if (e.IsBinary)
+            {
+                logger.AddLine("message was binary");
+            }
+            else if (e.IsPing)
+            {
+                logger.AddLine("message was ping");
+            }
+            else
+            {
+                logger.AddLine("Server sent a invalid messageq");
+            }
         }
     }
 }
