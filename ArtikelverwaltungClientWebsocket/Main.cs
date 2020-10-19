d806850d94b75666560f9518d3324869aa4807da
Nekromateion <nekromateion@web.de>
2020-10-19 03:40:28 +0200

    awww yeah socket goes brrrr

--- ArtikelverwaltungClientWebsocket/Main.cs
+++ ArtikelverwaltungClientWebsocket/Main.cs
@@ -1,12 +1,19 @@
-﻿using ArtikelverwaltungClientWebsocketLoader;
+﻿using System;
+using WebSocketSharp;
 
 namespace ArtikelverwaltungClientWebsocket
 {
+    public class ConnectionManager
+    {
+        public static WebSocket socket;
+    }
+    
     public class Main
     {
         public static void OnApplicationStart()
         {
-            
+            Console.WriteLine("Getting webhook from loader");
+            ConnectionManager.socket = ArtikelverwaltungClientWebsocketLoader.SocketManager.Socket;
         }
     }
 }
