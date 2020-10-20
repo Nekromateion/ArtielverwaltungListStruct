382f18454e38b169d75907d9386a24977cbd9933
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-20 20:51:55 +0200

    Create Article.cs

---
+++ ArtikelverwaltungWebSocketServer/Article.cs
@@ -0,0 +1,11 @@
+﻿namespace ArtikelverwaltungWebSocketServer
+{
+    public struct Article
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public int count { get; set; }
+        //public bool hasFile { get; set; } // this is used to store a file on the server (maybe a .pdf with extra infos) -> temporary not in use
+    }
+}
