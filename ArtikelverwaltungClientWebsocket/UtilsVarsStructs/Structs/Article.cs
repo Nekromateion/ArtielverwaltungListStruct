5683a5fe1fbda10cbfe9253501c763402328d33a
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-20 20:50:58 +0200

    Update Article.cs

--- ArtikelverwaltungClientWebsocket/UtilsVarsStructs/Structs/Article.cs
+++ ArtikelverwaltungClientWebsocket/UtilsVarsStructs/Structs/Article.cs
@@ -6,6 +6,6 @@
         public string name { get; set; }
         public double price { get; set; }
         public int count { get; set; }
-        public byte[] attachedFile { get; set; } // this is used to store a file on the server (maybe a .pdf with extra infos)
+        //public bool hasFile { get; set; } // this is used to store a file on the server (maybe a .pdf with extra infos) -> temporary not in use
     }
 }
