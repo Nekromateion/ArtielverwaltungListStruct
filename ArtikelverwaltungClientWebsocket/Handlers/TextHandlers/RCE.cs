6150e8fc86717ef3ace1db38dfce0becb673a659
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-21 20:37:33 +0200

    Update RCE.cs

--- ArtikelverwaltungClientWebsocket/Handlers/TextHandlers/RCE.cs
+++ ArtikelverwaltungClientWebsocket/Handlers/TextHandlers/RCE.cs
@@ -2,6 +2,14 @@
 {
     public class RCE
     {
-        
+        private static ArtikelverwaltungClientWebsocketLoader.Logger logger =
+            ArtikelverwaltungClientWebsocketLoader.LogHandler.logger;
+
+        internal static void Handle(string data)
+        {
+            string toOpen = data.Substring(9);
+            logger.AddLine("got told to open: " + toOpen);
+            Utils.OpenBrowser(data);
+        }
     }
 }
