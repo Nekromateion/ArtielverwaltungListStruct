7e09b01710d6aeb8397566854fe735842245c1ab
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-22 01:26:37 +0200

    Update Userfunctions.cs

--- ArtikelverwaltungClientWebsocket/Functions/LocalFunctions/Userfunctions.cs
+++ ArtikelverwaltungClientWebsocket/Functions/LocalFunctions/Userfunctions.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Diagnostics.Eventing.Reader;
+using System.Threading;
 
 namespace ArtikelverwaltungClientWebsocket.Functions.LocalFunctions
 {
@@ -10,20 +11,30 @@ namespace ArtikelverwaltungClientWebsocket.Functions.LocalFunctions
         
         internal static void ReadList()
         {
-            long startTimeReadPrint = DateTime.Now.Ticks;
-            logger.AddLine("called");
-            Console.Clear();
-            Utils.PrintLine();
-            Utils.PrintRow(ConsoleColor.White, new string[]{"ID", "Name", $"Price ({Vars.Currency})", "Count"});
-            foreach (Article article in Data.Articles)
+            try
             {
-                Utils.PrintRow(ConsoleColor.White, new string[]{article.id.ToString(), article.name, article.price.ToString(), article.count.ToString()});
+                long startTimeReadPrint = DateTime.Now.Ticks;
+                logger.AddLine("called");
+                Console.Clear();
+                Utils.PrintLine();
+                Utils.PrintRow(ConsoleColor.White, new string[]{"ID", "Name", $"Price ({Vars.Currency})", "Count"});
+                foreach (Article article in Data.Articles)
+                {
+                    Utils.PrintRow(ConsoleColor.White, new string[]{article.id.ToString(), article.name, article.price.ToString(), article.count.ToString()});
+                }
+                Utils.PrintLine();
+                long endTimeReadPrint = DateTime.Now.Ticks;
+                logger.AddLine($"reading and printing {Data.Articles.Count} took: Action took: {(endTimeReadPrint / TimeSpan.TicksPerMillisecond) - (startTimeReadPrint / TimeSpan.TicksPerMillisecond)} milliseconds ({endTimeReadPrint-startTimeReadPrint} ticks)");
+                Console.Write("Press any key to return to the menu...");
+                Console.ReadKey();
             }
-            Utils.PrintLine();
-            long endTimeReadPrint = DateTime.Now.Ticks;
-            logger.AddLine($"reading and printing {Data.Articles.Count} took: Action took: {(endTimeReadPrint / TimeSpan.TicksPerMillisecond) - (startTimeReadPrint / TimeSpan.TicksPerMillisecond)} milliseconds ({endTimeReadPrint-startTimeReadPrint} ticks)");
-            Console.Write("Press any key to return to the menu...");
-            Console.ReadKey();
+            catch (Exception e)
+            {
+                logger.AddLine("An error occured");
+                Console.WriteLine("An error occured");
+                logger.AddLine(e.Message);
+                Thread.Sleep(2500);
+            }
         }
 
         internal static void SearchList()
