8f5eaf56b389f7702e2e887d80b7375e3275c39a
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-21 19:55:13 +0200

    Update Adminfunctions.cs

--- ArtikelverwaltungClientWebsocket/Functions/ServerFunctions/Adminfunctions.cs
+++ ArtikelverwaltungClientWebsocket/Functions/ServerFunctions/Adminfunctions.cs
@@ -2,6 +2,19 @@ namespace ArtikelverwaltungClientWebsocket.Functions.ServerFunctions
 {
     public class Adminfunctions
     {
-        
+        internal static void CloseServer()
+        {
+            
+        }
+
+        internal static void SaveList()
+        {
+            
+        }
+
+        internal static void ClearList()
+        {
+            
+        }
     }
 }
