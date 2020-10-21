95022f0917a7eec6f907921ee44743837701d873
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-22 01:20:51 +0200

    Update Userfunctions.cs

--- ArtikelverwaltungClientWebsocket/Functions/LocalFunctions/Userfunctions.cs
+++ ArtikelverwaltungClientWebsocket/Functions/LocalFunctions/Userfunctions.cs
@@ -1,12 +1,23 @@
+using System;
 using System.Diagnostics.Eventing.Reader;
 
 namespace ArtikelverwaltungClientWebsocket.Functions.LocalFunctions
 {
     public class Userfunctions
     {
+        private static ArtikelverwaltungClientWebsocketLoader.Logger logger =
+            ArtikelverwaltungClientWebsocketLoader.LogHandler.logger;
+        
         internal static void ReadList()
         {
-            
+            logger.AddLine("called");
+            Utils.PrintLine();
+            Utils.PrintRow(ConsoleColor.White, new string[]{"ID", "Name", $"Price ({Vars.Currency})", "Count"});
+            foreach (Article article in Data.Articles)
+            {
+                Utils.PrintRow(ConsoleColor.White, new string[]{article.id.ToString(), article.name, article.price.ToString(), article.count.ToString()});
+            }
+            Utils.PrintLine();
         }
 
         internal static void SearchList()
