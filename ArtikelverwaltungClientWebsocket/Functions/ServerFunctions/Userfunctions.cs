5f9374b351041376e0efa44c174cf6ef209e791e
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-21 23:19:54 +0200

    Update Userfunctions.cs

--- ArtikelverwaltungClientWebsocket/Functions/ServerFunctions/Userfunctions.cs
+++ ArtikelverwaltungClientWebsocket/Functions/ServerFunctions/Userfunctions.cs
@@ -2,19 +2,24 @@ namespace ArtikelverwaltungClientWebsocket.Functions.ServerFunctions
 {
     public class Userfunctions
     {
+        private static ArtikelverwaltungClientWebsocketLoader.Logger logger =
+            ArtikelverwaltungClientWebsocketLoader.LogHandler.logger;
         internal static void InitList()
         {
+            logger.AddLine("called");
             ConnectionManager.socket.Send("request data");
         }
 
         internal static void InitCurrency()
         {
+            logger.AddLine("called");
             ConnectionManager.socket.Send("get currency");
         }
 
         internal static void RequestStatusBroadcast()
         {
-            
+            logger.AddLine("called");
+            ConnectionManager.socket.Send("broadcast status");
         }
     }
 }
