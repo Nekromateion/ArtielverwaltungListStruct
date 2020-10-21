e947b8aba7cb4400208ca882643b84acaaa70470
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-21 23:19:58 +0200

    Update Adminfunctions.cs

--- ArtikelverwaltungClientWebsocket/Functions/ServerFunctions/Adminfunctions.cs
+++ ArtikelverwaltungClientWebsocket/Functions/ServerFunctions/Adminfunctions.cs
@@ -2,6 +2,9 @@ namespace ArtikelverwaltungClientWebsocket.Functions.ServerFunctions
 {
     public class Adminfunctions
     {
+        private static ArtikelverwaltungClientWebsocketLoader.Logger logger =
+            ArtikelverwaltungClientWebsocketLoader.LogHandler.logger;
+        
         internal static void CloseServer()
         {
             
