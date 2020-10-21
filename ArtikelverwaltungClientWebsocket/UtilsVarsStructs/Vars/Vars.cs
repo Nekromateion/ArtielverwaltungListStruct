6c41236bc4bc9d8d3d3e3c1b42ae65cdacadc405
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-21 03:05:43 +0200

    Update Vars.cs

--- ArtikelverwaltungClientWebsocket/UtilsVarsStructs/Vars/Vars.cs
+++ ArtikelverwaltungClientWebsocket/UtilsVarsStructs/Vars/Vars.cs
@@ -6,5 +6,7 @@ namespace ArtikelverwaltungClientWebsocket
         public static string AdminKey { get; set; }
         public static string EditKey { get; set; }
         public static int ConnectedUsers { get; set; }
+        
+        public static string Currency { get; set; }
     }
 }
