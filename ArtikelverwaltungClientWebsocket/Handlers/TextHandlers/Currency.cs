5f75957808a892c99c7fc7bb84c0275f2ebbe5a1
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-21 20:35:00 +0200

    Update Currency.cs

--- ArtikelverwaltungClientWebsocket/Handlers/TextHandlers/Currency.cs
+++ ArtikelverwaltungClientWebsocket/Handlers/TextHandlers/Currency.cs
@@ -2,9 +2,23 @@
 {
     public class Currency
     {
+        private static ArtikelverwaltungClientWebsocketLoader.Logger logger =
+            ArtikelverwaltungClientWebsocketLoader.LogHandler.logger;
+        
         internal static void Handle(string data)
         {
-            
+            logger.AddLine("message was currency info");
+            string currency = data.Substring(12);
+            if (Vars.Currency == null)
+            {
+                logger.AddLine("server uses currency: " + currency);
+                Vars.Currency = currency;
+            }
+            else
+            {
+                logger.AddLine("server switched currency to: " + currency);
+                Vars.Currency = currency;
+            }
         }
     }
 }
