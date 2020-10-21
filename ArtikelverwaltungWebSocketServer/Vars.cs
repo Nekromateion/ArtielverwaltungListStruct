e7fdc61158cf85fe79ea37d2bab20cadefd7605b
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-21 22:14:46 +0200

    Update Vars.cs

--- ArtikelverwaltungWebSocketServer/Vars.cs
+++ ArtikelverwaltungWebSocketServer/Vars.cs
@@ -3,5 +3,9 @@
     public class Vars
     {
         public static string Currency { get; set; }
+        
+        public static string AdminKey { get; set; }
+        
+        public static string EditKey { get; set; }
     }
 }
