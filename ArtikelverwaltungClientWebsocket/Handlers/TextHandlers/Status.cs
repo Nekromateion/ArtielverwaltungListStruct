6aadefe225e7cda5c7f751149e188f227a2b758b
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-21 20:34:15 +0200

    Update Status.cs

--- ArtikelverwaltungClientWebsocket/Handlers/TextHandlers/Status.cs
+++ ArtikelverwaltungClientWebsocket/Handlers/TextHandlers/Status.cs
@@ -1,10 +1,19 @@
-﻿namespace ArtikelverwaltungClientWebsocket.Handlers.TextHandlers
+﻿using System;
+
+namespace ArtikelverwaltungClientWebsocket.Handlers.TextHandlers
 {
     public class Status
     {
+        private static ArtikelverwaltungClientWebsocketLoader.Logger logger =
+            ArtikelverwaltungClientWebsocketLoader.LogHandler.logger;
         internal static void Handle(string data)
         {
-            
+            logger.AddLine("message was status");
+            string message = data.Substring(6);
+            logger.AddLine("Status message: " + message);
+            string[] numbers = data.Split(' ');
+            Vars.ConnectedUsers = Convert.ToInt32(numbers[2].Replace(" ", string.Empty));
+            Console.Title = $"Article management v{Vars.Version} | Connected users: {Vars.ConnectedUsers}";
         }
     }
 }
