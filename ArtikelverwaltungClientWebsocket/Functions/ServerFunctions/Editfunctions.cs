0ba4842932b350dfb9cdee3219fdbfd29b179449
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-22 23:44:35 +0200

    Update Editfunctions.cs

--- ArtikelverwaltungClientWebsocket/Functions/ServerFunctions/Editfunctions.cs
+++ ArtikelverwaltungClientWebsocket/Functions/ServerFunctions/Editfunctions.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ArtikelverwaltungClientWebsocket.Functions.ServerFunctions
 {
     public class Editfunctions
@@ -7,7 +9,32 @@ namespace ArtikelverwaltungClientWebsocket.Functions.ServerFunctions
         
         internal static void AddArticle()
         {
+            bool didWork = false;
             logger.AddLine("called");
+            string id;
+            string name;
+            double price;
+            int count;
+            while (!didWork)
+            {
+                Console.Clear();
+                try
+                {
+                    Console.WriteLine("Please enter some information about the article you want to add");
+                    Console.Write("ID(A = Auto): ");
+                    id = Console.ReadLine();
+                    Console.Write("Name: ");
+                    name = Console.ReadLine();
+                    Console.Write("Price: ");
+                    price = Convert.ToDouble(Console.ReadLine());
+                    Console.Write("Count: ");
+                    count = Convert.ToInt32(Console.ReadLine());
+                    didWork = true;
+                }
+                catch (Exception) { }
+                Console.Clear();
+            }
+            ConnectionManager.socket.Send("");
         }
 
         internal static void RemoveArticle()
