eefbabfcc3b18f96652ac0f8e5cf283be3cbb33a
Nekromateion <43814053+Nekromateion@users.noreply.github.com>
2020-10-28 23:43:30 +0100

    brrrrr

--- ArtikelverwaltungWebSocketServer/Discord/Env/UserAdd.cs
+++ ArtikelverwaltungWebSocketServer/Discord/Env/UserAdd.cs
@@ -2,6 +2,10 @@
 {
     public struct UserAdd
     {
-        
+        public static ulong UserId { get; set; }
+        public static int Id { get; set; }
+        public static string name { get; set; }
+        public static double price { get; set; }
+        public static int Count { get; set; }
     }
 }
